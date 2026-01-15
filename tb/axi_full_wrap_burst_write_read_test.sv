1827b804c2d52ae22efcfae689c78bb790c53917
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-01-15 19:21:49 +0530

    Create axi_full_wrap_burst_write_read_test.sv
    AXI full wrap burst test case with write followed by read

---
+++ tb/axi_full_wrap_burst_write_read_test.sv
@@ -0,0 +1,32 @@
+/**********************************************/
+/* Testname: axi_full_wrap_burst_write_read_test
+/* Description: This is a write and read operation testcase for AXI-Full wrap enviroment.
+/**********************************************/
+`include "uvm_macros.svh"
+
+import uvm_pkg::*;
+import axi_full_pkg::*;
+import axi_full_uvm_pkg::*;  
+import axi_full_tb_pkg::*;
+class axi_full_wrap_burst_write_read_test extends axi_full_base_test;
+ `uvm_component_utils(axi_full_wrap_burst_write_read_test)
+  
+  axi_wrap_burst_write_read_sequence axi_wrap_wr_rd_seq;
+
+  function new(string name = "axi_full_wrap_burst_write_read_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction: new
+
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+	axi_wrap_wr_rd_seq = axi_wrap_burst_write_read_sequence::type_id::create("axi_wrap_wr_rd_seq", this);
+  endfunction: build_phase
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+      axi_wrap_wr_rd_seq.start(axi_env_0.axi_agent_0.axi_sequencer_0);
+	phase.drop_objection(this);
+  endtask: run_phase
+
+endclass: axi_full_wrap_burst_write_read_test
