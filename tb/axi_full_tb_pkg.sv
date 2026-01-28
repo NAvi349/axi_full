09b8ab07c5d20793d988aeaa5ba13d752049623e
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-01-28 22:31:21 +0530

    Update axi_full_tb_pkg.sv
    Added multi-id test name

--- tb/axi_full_tb_pkg.sv
+++ tb/axi_full_tb_pkg.sv
@@ -5,5 +5,8 @@ package axi_full_tb_pkg;
   `include "axi_full_read_test.sv"
   `include "axi_full_write_test.sv"
   `include "axi_full_write_read_test.sv"
+  //`include "axi_full_fixed_burst_write_read_test.sv"
   `include "axi_full_incr_burst_write_read_test.sv"
+  `include "axi_full_wrap_burst_write_read_test.sv"
+  `include "axi_full_multi_id_write_read_test.sv"
 endpackage
